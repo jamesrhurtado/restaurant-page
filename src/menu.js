9bb9195b74ccd10c5dd90c41a7b4e22e9619ce6a
jrenzzo <renzo-hg03@hotmail.com>
2022-03-06 16:03:38 -0500

    upload imgs and added menu functions

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,32 @@
 function addMenu(){
+    const menu = document.createElement('div')
+    menu.classList.add('menu')
+    
 
+
+
+}
+
+function addMenuItem(name, description){
+    const cardMenu = document.createElement('div')
+    cardMenu.classList.add('menu-container')
+
+    const pizzaName = document.createElement('h3')
+    pizzaName.textContent = name
+
+    const pizzaDes = document.createElement('p')
+    pizzaDes.textContent = description
+
+    const pizzaImg = document.createElement('img')
+    pizzaImg.src = `images/${name.toLowerCase()}.jpg`
+    pizzaImg.alt = `${name}`
+
+    cardMenu.appendChild(pizzaImg)
+    cardMenu.appendChild(pizzaName)
+    cardMenu.appendChild(pizzaDes)
+    
+    return cardMenu
+    
 }
 
 function setUpMenu(){
